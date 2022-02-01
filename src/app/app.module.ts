d19e713450d745e9e9c11f916d1a484861daafa6
Paige <peh0015@mix.wvu.edu>
2022-02-01 14:25:31 -0500

    card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TopNavBarComponent } from 'src/navigation/topnavbar.component';
+import { CardComponent } from 'src/card/card.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
